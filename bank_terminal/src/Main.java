a24c5412c26a8a6f04f762e4df930315f16c0269
Vadim Pozdnyakov <99559167+vvp1061@users.noreply.github.com>
2022-12-27 23:31:53 +0300

    Add files via upload

---
+++ bank_terminal/src/Main.java
@@ -0,0 +1,20 @@
+import BankingTerminalClient.NewJFrame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        NewJFrame mainFrame = new NewJFrame();
+        mainFrame.setVisible(true);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.setSize(520,670);
+        mainFrame.setResizable(false);
+        mainFrame.setLocationRelativeTo(null);
+    }
+
+}
+
+
+
+
+
